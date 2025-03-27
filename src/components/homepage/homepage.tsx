2b7bff15d50c69fba950f7ab9a35a4ebac14efb8
Quinn <101746@glr.nl>
2025-03-27 11:34:05 +0100

    initial commit

---
+++ src/components/homepage/homepage.tsx
@@ -0,0 +1,12 @@
+import Main_title from "./main_title/main_title";
+
+function Homepage() {
+  return (
+    <div>
+      <Main_title />
+    </div>
+  );
+}
+
+
+export default Homepage;
---
+++ src/components/homepage/main_title/main_title.tsx
@@ -0,0 +1,10 @@
+
+function Main_title() {
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default Main_title;
